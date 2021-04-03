cd1aecbf80b8c3dd5963bdb3942a76aff223529e
Akash <akashverma265@gmail.com>
2021-04-03 18:01:31 +0530

    This is my Global command

---
+++ fs.js
@@ -0,0 +1,9 @@
+let fs = require("fs");
+/*
+// write file 
+fs.writeFileSync("a.txt" , "This is overwrite function!");
+//folder create
+fs.mkdirSync("abc");
+// remove folder
+fs.rmdirSync("abc");
+*/
